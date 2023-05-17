02a695fe5c6ebc0d0bd31300214a6eecbe2487aa
claudi_tm <ccaducarlos@gmail>
2023-05-17 19:51:02 +0200

    created routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("working");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", () => {
+    return "hello world"
+})
+
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log("HTTP server running");
+    
+})
