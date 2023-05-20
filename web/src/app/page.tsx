913eeff740537cf21014964165fc1b291877d138
claudi_tm <ccaducarlos@gmail>
2023-05-20 07:47:44 +0200

    fonts configurated

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text-2xl text-orange-500">Hello world</h1>
+  return (
+    <>
+      <h1 className="text-2xl text-orange-500 font-alt">Hello world</h1>
+      <h1 className="text-2xl text-orange-500 font-sans">Hello world</h1>
+    </>
+  )
 }
