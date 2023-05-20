efd86b723c051770e81778a24b9489d5e6998bdd
claudi_tm <ccaducarlos@gmail>
2023-05-20 09:10:35 +0200

    created stripes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,30 @@
 export default function Home() {
   return (
-    
+    <>
+      <main className="grid grid-cols-2 min-h-screen">
+        {/* Left */}
+
+        <div className="flex flex-col items-start justify-between px-28 py-16 relative border-r border-white/10">
+          {/* blur */}
+          <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 overflow-hidden  rounded-full blur-full bg-purple-700 opacity-50"></div>
+          {/* stripes */}
+          <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
+        </div>
+
+        {/* Right */}
+        <div className="flex flex-col p-16">
+          <div className="flex flex-1 items-center justify-center">
+            <p className="text-center leading-relaxed w-[360px]">
+              Você ainda não registrou nenhuma lembrança, comece a
+              <a href="" className="underline hover:text-gray-50">
+                {' '}
+                criar agora
+              </a>
+              !
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
