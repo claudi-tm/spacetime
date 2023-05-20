ae2a0102fa8f1827b842147afe03c9e003ed1fc0
claudi_tm <ccaducarlos@gmail>
2023-05-20 08:05:35 +0200

    configurated colors

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,5 @@
 export default function Home() {
   return (
-    <>
-      <h1 className="text-2xl text-orange-500 font-alt">Hello world</h1>
-      <h1 className="text-2xl text-orange-500 font-sans">Hello world</h1>
-    </>
+    
   )
 }
